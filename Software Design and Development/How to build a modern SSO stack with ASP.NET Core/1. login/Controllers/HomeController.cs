3b7e5c505beabd794e1a729f17660a482ec6c70c
Scott Brady <scott@scottbrady91.com>
2022-05-13 12:56:08 +0100

    Initial identity login example

--- Software Design and Development/How to build a modern SSO stack with ASP.NET Core/1. login/Controllers/HomeController.cs
+++ Software Design and Development/How to build a modern SSO stack with ASP.NET Core/1. login/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyWebsite.Models;
 
@@ -6,18 +7,12 @@ namespace MyWebsite.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
 
+    [Authorize]
     public IActionResult Privacy()
     {
         return View();
--- Software Design and Development/How to build a modern SSO stack with ASP.NET Core/1. login/Program.cs
+++ Software Design and Development/How to build a modern SSO stack with ASP.NET Core/1. login/Program.cs
@@ -1,17 +1,44 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MyWebsite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews()
     .AddRazorRuntimeCompilation();
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=./identity.db"));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.Name = "test";
+    options.SlidingExpiration
+});
+
 var app = builder.Build();
 
 app.UseDeveloperExceptionPage();
+
+// bootstrap database & user - DEMO ONLY
+using (var services = app.Services.CreateScope())
+{
+    var identityContext = services.ServiceProvider.GetRequiredService<AppDbContext>();
+    identityContext.Database.EnsureCreated();
+
+    var bootstrapUser = new IdentityUser("scott") {Id = "854fbc85-2c61-4e93-a0a6-060f54e86367", Email = "scott@scottbrady91.com", EmailConfirmed = true};
+    var userManager = services.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    if(!identityContext.Users.Any()) userManager.CreateAsync(bootstrapUser, "Password123!");    
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
